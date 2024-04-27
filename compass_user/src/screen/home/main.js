4402ff233ff93debd95e08bfa67fb7a9cbcce31b
keero <47709146+keero1@users.noreply.github.com>
2024-04-27 21:19:27 +0800

    Added map and location permission prompt

--- compass_user/src/screen/home/main.js
+++ compass_user/src/screen/home/main.js
@@ -1,37 +1,111 @@
-import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  Dimensions,
-} from 'react-native';
+import React, {useRef, useEffect, useState} from 'react';
+import {StyleSheet, View, TouchableOpacity, Dimensions} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-
-import {COLORS} from '../../constants';
 
 // ROUTES
 
 import ROUTES from '../../constants/routes';
 
+// MAP
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+import Geolocation from '@react-native-community/geolocation';
+
 const Main = props => {
   const {navigation} = props;
+  const mapRef = useRef(MapView);
 
+  // user current location
+  const [initialRegion, setInitialRegion] = useState(null);
+  const [currentLocation, setCurrentLocation] = useState(null);
+  const [mapRegion, setMapRegion] = useState(null);
+
+  useEffect(() => {
+    // Fetch current location when component mounts
+    Geolocation.getCurrentPosition(
+      position => {
+        const {latitude, longitude} = position.coords;
+        setCurrentLocation({latitude, longitude});
+
+        setInitialRegion({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        });
+      },
+      error => {
+        console.error(error);
+      },
+      {enableHighAccuracy: true, timeout: 20000},
+    );
+  }, []); // Empty dependency array to run only on mount
+
+  // hamburger menu
   const onMenuPressed = () => {
     navigation.navigate(ROUTES.DRAWER);
 
-    console.log("DRAWER");
+    console.log('DRAWER');
+  };
+
+  // reset camera to north
+  const resetRotation = () => {
+    console.log("Rotation Reset")
+    if (mapRef.current) {
+      mapRef.current.animateCamera({heading: 0});
+    }
+  };
+
+  // handle centering to user
+  const centerToUser = () => {
+    console.log("Reset Camera to User")
+    if (currentLocation && mapRef.current) {
+      mapRef.current.animateToRegion({
+        latitude: currentLocation.latitude,
+        longitude: currentLocation.longitude,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+      });
+    }
+  };
+
+  // save the current region
+  const onRegionChangeComplete = region => {
+    setMapRegion(region);
   };
 
   return (
-    <SafeAreaView style={styles.main}>
-      <TouchableOpacity onPress={onMenuPressed}>
-        <View style={styles.hamburgerMenu}>
-          <Icon name="menu" size={30} color="black" />
-        </View>
-      </TouchableOpacity>
-      <View style={styles.root}>{<Text> GoogleMap_API [ERROR] CODE 400</Text>}</View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={onMenuPressed}>
+          <View style={styles.hamburgerMenu}>
+            <Icon name="menu" size={30} color="black" />
+          </View>
+        </TouchableOpacity>
+      </View>
+      {/* 14.823320026254835, 121.05946449733479 */}
+      <View style={styles.mapContainer}>
+        <MapView
+          ref={mapRef}
+          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+          style={styles.map}
+          showsUserLocation={true}
+          showsMyLocationButton={false}
+          initialRegion={initialRegion}
+          maxZoomLevel={20}
+          minZoomLevel={12}
+          onRegionChangeComplete={onRegionChangeComplete}
+          showsCompass={false}
+        />
+        {/* compass button */}
+        <TouchableOpacity onPress={resetRotation} style={styles.compassButton}>
+          <Icon name="navigation" size={30} color="black" />
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={centerToUser} style={styles.centerButton}>
+          <Icon name="my-location" size={30} color="black" />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
@@ -42,28 +116,56 @@ const screenWidth = Dimensions.get('window').width;
 const screenHeight = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
-  main: {
+  container: {
     flex: 1,
     backgroundColor: '#F4F4FB',
   },
-  hamburgerMenu: {
+  header: {
     position: 'absolute',
     top: 20,
     left: 20,
     zIndex: 999,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  hamburgerMenu: {
     backgroundColor: '#e4e9f6',
     justifyContent: 'center',
     alignItems: 'center',
     width: screenWidth * 0.112,
     height: screenHeight * 0.061,
-
     borderRadius: 10,
+    marginRight: 10,
   },
-  root: {
-    flex: 1,
-    justifyContent: 'space-between',
-    flexGrow: 1,
+  mapContainer: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+  },
+  compassButton: {
+    backgroundColor: '#e4e9f6',
+    justifyContent: 'center',
     alignItems: 'center',
-    padding: 20,
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    position: 'absolute',
+    top: '20%', // 20% higher from the center
+    right: 10, // Right side of the screen
+  },
+
+  centerButton: {
+    backgroundColor: '#e4e9f6',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
