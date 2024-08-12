0454dd062c97de39f9d8c2531754743c29f70e82
keero <47709146+keero1@users.noreply.github.com>
2024-08-13 01:21:53 +0800

    configure firebase/googlemap apis and gradle stuffs. also changed the app.tsx to .js

---
+++ compass_main/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+} from 'react-native';
+
+
+export default function App() {
+  return <Text>HELLO</Text>;
+}
+
+const styles = StyleSheet.create({
+
+});
