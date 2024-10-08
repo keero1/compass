34f5c05a529215344cbdcafa62dfc6d20c074f5d
keero <47709146+keero1@users.noreply.github.com>
2024-10-08 13:03:24 +0800

    added theme context

---
+++ compass_admin/src/contexts/themeContext/index.js
@@ -0,0 +1,22 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "nord");
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+  return useContext(ThemeContext);
+};
