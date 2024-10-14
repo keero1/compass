0af719f2031183e4a867dbff4f2fc5e3b76b03db
keero <47709146+keero1@users.noreply.github.com>
2024-10-14 16:58:54 +0800

    added app icon in bus app

--- compass_main/src/constants/images.js
+++ compass_main/src/constants/images.js
@@ -1,4 +1,6 @@
 export default {
-    logo: require('../../assets/images/frieren.png'),
+    logo: require('../../assets/images/logo2.png'),
+    bus_icon1: require('../../assets/images/bus1.png'),
+    bus_icon2: require('../../assets/images/bus2.png'),
   };
   
